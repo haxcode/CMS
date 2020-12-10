3ba5d80df4e9e08f0f4bd454c6e5a56c5172c64b
rkubica <rkubica@edokumenty.eu>
2020-12-10 20:55:52 +0100

    UUID enhancement

--- src/Common/UUID.php
+++ src/Common/UUID.php
@@ -4,22 +4,58 @@ namespace App\Common;
 
 use Symfony\Component\Uid\Uuid as UID;
 
+/**
+ * Class UUID
+ *
+ * @package          App\Common
+ * @createDate       2020-12-10
+ * @author           Robert Kubica <rkubica@edokumenty.eu>
+ * @copyright (c)    eDokumenty Sp. z o.o.
+ */
 final class UUID {
 
+    /**
+     * @var string|\Symfony\Component\Uid\AbstractUid|UID
+     */
     private string $value;
 
+    /**
+     * UUID constructor.
+     *
+     * @param string $value
+     */
     public function __construct(string $value) {
         $this->value = UID::fromString($value);
     }
 
+    /**
+     * @return static
+     */
     public static function random(): self {
         return new self(UID::v4());
     }
 
+    /**
+     * @param string $uuid
+     *
+     * @return bool
+     */
+    public static function valid(string $uuid): bool {
+        return UID::isValid($uuid);
+    }
+
+    /**
+     * @return string
+     */
     public function __toString(): string {
         return $this->value;
     }
 
+    /**
+     * @param UUID $id
+     *
+     * @return bool
+     */
     public function isEqual(self $id): bool {
         return $this->value === $id->value;
     }
