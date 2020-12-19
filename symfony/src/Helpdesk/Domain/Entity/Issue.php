3c8d27c19e41249c35242ee66f4ee50be8446cc7
rkubica <rkubica@edokumenty.eu>
2020-12-19 12:51:24 +0100

    Extends Issue to almost final form

--- symfony/src/Helpdesk/Domain/Entity/Issue.php
+++ symfony/src/Helpdesk/Domain/Entity/Issue.php
@@ -4,6 +4,9 @@ namespace App\Helpdesk\Domain\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Uid\Uuid;
+use Doctrine\ORM\Mapping\OneToOne;
+use Doctrine\ORM\Mapping\JoinColumn;
+use App\Client\Domain\Entity\Client;
 
 /**
  * @ORM\Entity(repositoryClass="App\Helpdesk\Infrastructure\Repository\IssueRepository")
@@ -17,10 +20,132 @@ class Issue {
     private Uuid $issueId;
 
     /**
+     * @OneToOne(targetEntity="App\Client\Domain\Entity\Client")
+     * @JoinColumn(name="client_uuid", referencedColumnName="id")
+     */
+    private Client $client;
+
+    /**
+     * @ORM\Column(type="text",name="title")
+     */
+    private string $title;
+
+    /**
+     * @ORM\Column(type="text",name="description")
+     */
+    private string $description;
+
+    /**
+     * @ORM\Column(type="bigint",name="usr_id")
+     */
+    private int $author;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private string $importance;
+
+    /**
+     * @ORM\Column(type="boolean", name="is_confidential")
+     */
+    private string $confidential;
+
+    /**
      * @return Uuid
      */
     public function getIssueId(): Uuid {
         return $this->issueId;
+    }
+
+    /**
+     * @param Uuid $issueId
+     */
+    public function setIssueId(Uuid $issueId): void {
+        $this->issueId = $issueId;
+    }
+
+    /**
+     * @return Client
+     */
+    public function getClient(): Client {
+        return $this->client;
+    }
+
+    /**
+     * @param Client $client
+     */
+    public function setClient(Client $client): void {
+        $this->client = $client;
+    }
+
+    /**
+     * @return string
+     */
+    public function getConfidential(): string {
+        return $this->confidential;
+    }
+
+    /**
+     * @param string $confidential
+     */
+    public function setConfidential(string $confidential): void {
+        $this->confidential = $confidential;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     */
+    public function setTitle(string $title): void {
+        $this->title = $title;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription(): string {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     */
+    public function setDescription(string $description): void {
+        $this->description = $description;
+    }
+
+    /**
+     * @return int
+     */
+    public function getAuthor(): int {
+        return $this->author;
+    }
+
+    /**
+     * @param int $author
+     */
+    public function setAuthor(int $author): void {
+        $this->author = $author;
+    }
+
+    /**
+     * @return string
+     */
+    public function getImportance(): string {
+        return $this->importance;
+    }
+
+    /**
+     * @param string $importance
+     */
+    public function setImportance(string $importance): void {
+        $this->importance = $importance;
     }
 
 }
