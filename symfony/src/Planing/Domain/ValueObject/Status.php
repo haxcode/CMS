589a0b58f7c1bb270bdcfcabcdd9505d09b6818a
rkubica <rkubica@edokumenty.eu>
2021-01-06 10:12:37 +0100

    Added state for task in task domain

---
+++ symfony/src/Planing/Domain/ValueObject/Status.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Planing\Domain\ValueObject;
+
+use App\Common\Exception\NotSupportedType;
+use Stringable;
+
+class Status implements Stringable {
+
+    public const BACKLOG = 'BACKLOG';
+    public const WEEKLY = 'WEEKLY';
+    public const DAILY = 'DAILY';
+    public const VERIFICATION = 'VERIFICATION';
+    public const DONE = 'DONE';
+    private string $guid;
+
+    public function __construct(string $guid = self::BACKLOG) {
+        if (!self::isValid($guid)) {
+            throw new NotSupportedType('Not supported type of state');
+        }
+        $this->guid = $guid;
+    }
+
+    public static function getTypes(): array {
+        return [
+            self::BACKLOG      => 'added to backlog',
+            self::WEEKLY       => 'planed in this sprint',
+            self::DAILY        => 'in progres',
+            self::VERIFICATION => 'done - in verification stage',
+            self::DONE         => 'done',
+        ];
+    }
+
+    /**
+     * @param string $guid
+     *
+     * @return bool
+     */
+    public static function isValid(string $guid): bool {
+        return in_array($guid, array_keys(self::getTypes()));
+    }
+
+    /**
+     * @return string
+     */
+    public function getLevelName(): string {
+        return self::getTypes()[$this->guid];
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString(): string {
+        return $this->guid;
+    }
+
+}
