7b62d8f186bc60877b1dfac5b24822947a9a11ab
rkubica <rkubica@edokumenty.eu>
2021-01-06 13:06:43 +0100

    DI to controller not to method

--- symfony/src/User/Controller/UserController.php
+++ symfony/src/User/Controller/UserController.php
@@ -5,12 +5,27 @@ namespace App\User\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
-use App\User\Entity\User;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use App\User\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Security;
 
 class UserController extends AbstractController {
+
+    /**
+     * @var UserRepository
+     */
+    private UserRepository $userRepository;
+    /**
+     * @var Security
+     */
+    private Security $security;
+
+    public function __construct(UserRepository $userRepository, Security $security) {
+
+        $this->userRepository = $userRepository;
+        $this->security = $security;
+    }
 
     /**
      * @Route("/api/admin/users", name="users_list")
@@ -20,8 +35,7 @@ class UserController extends AbstractController {
      */
     public function listUsers(): Response {
 
-        /** @var User[] $users */
-        $users = $this->getDoctrine()->getRepository(User::class)->findAll();
+        $users = $this->userRepository->findAll();
 
         return $this->json($users, Response::HTTP_OK);
 
@@ -34,11 +48,25 @@ class UserController extends AbstractController {
      *
      * @return JsonResponse
      */
-    public function getUserDetails(int $id, UserRepository $repository): Response {
+    public function getUserDetails(int $id): Response {
 
-        $user = $repository->find($id);
+        $user = $this->userRepository->find($id);
         if (!$user) {
             return $this->json(['error' => 'No user found for id '.$id], Response::HTTP_NOT_FOUND);
+        }
+        return $this->json($user, Response::HTTP_OK);
+    }
+
+    /**
+     * @Route("/api/userprofile", name="user_profile_details", methods={"GET"})
+     * @param Request $request
+     *
+     * @return JsonResponse
+     */
+    public function getUserProfileDetails(Request $request): Response {
+        $user = $this->userRepository->find($this->security->getUser()->getId());
+        if (!$user) {
+            return $this->json(['error' => 'Your session expired'], Response::HTTP_NOT_FOUND);
         }
         return $this->json($user, Response::HTTP_OK);
     }
