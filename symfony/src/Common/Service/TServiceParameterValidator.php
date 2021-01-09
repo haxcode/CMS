0ce316deded3b617f5642a7e78a399542ca05fcd
rkubica <rkubica@edokumenty.eu>
2021-01-09 12:24:48 +0100

    Uuid validation in service

--- symfony/src/Common/Service/TServiceParameterValidator.php
+++ symfony/src/Common/Service/TServiceParameterValidator.php
@@ -4,10 +4,11 @@ namespace App\Common\Service;
 
 use App\Common\Exception\Services\ServiceParameterRequiredException;
 use App\Common\Exception\Services\ServiceTypeParameterException;
+use Symfony\Component\Uid\Uuid;
 
 trait TServiceParameterValidator {
 
-    static string $SERVICE_NAME = 'SERVICE';
+    protected string $serviceName = 'SERVICE';
 
     /**
      * @param array $data
@@ -36,16 +37,19 @@ trait TServiceParameterValidator {
     private function validateRules($data, string $rule, string $alias): void {
         $rules = explode('|', $rule);
         if (in_array('required', $rules) && ($data == null)) {
-            throw new ServiceParameterRequiredException(self::$SERVICE_NAME, $alias);
+            throw new ServiceParameterRequiredException($this->serviceName, $alias);
         }
         if (in_array('text', $rules) && (!is_string($data) || empty($data))) {
-            throw new ServiceTypeParameterException(self::$SERVICE_NAME, $alias, 'text');
+            throw new ServiceTypeParameterException($this->serviceName, $alias, 'text');
         }
         if (in_array('int', $rules) && (!filter_var($data, FILTER_VALIDATE_INT))) {
-            throw new ServiceTypeParameterException(self::$SERVICE_NAME, $alias, 'integer');
+            throw new ServiceTypeParameterException($this->serviceName, $alias, 'integer');
         }
         if (in_array('email', $rules) && (!filter_var($data, FILTER_VALIDATE_EMAIL))) {
-            throw new ServiceTypeParameterException(self::$SERVICE_NAME, $alias, 'email');
+            throw new ServiceTypeParameterException($this->serviceName, $alias, 'email');
+        }
+        if (in_array('uuid', $rules) && (!Uuid::isValid($data))) {
+            throw new ServiceTypeParameterException($this->serviceName, $alias, 'uuid');
         }
     }
 
